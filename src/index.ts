130c58da4a60fa25d95e3f1867a0757cbf26cc43
is386 <singh.indervir89@gmail.com>
2026-03-07 23:36:57 -0500

    handle numbers, contractions, and special chars

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import wordlist from "wordlist-english";
 import * as fs from "node:fs";
+import { contractions } from "./contractions.js";
 
-const englishWords = wordlist.english;
+const englishWords = wordlist.english.concat(contractions);
+
+// TODO: read in file from args
+// TODO: read in multiple files from args
+// TODO: read in stdin for piping
+// TODO: custom words
 
 fs.readFile(
-  "test.txt",
+  "example.txt",
   "utf8",
   (err: NodeJS.ErrnoException | null, data: string) => {
     if (err) {
@@ -17,22 +23,50 @@ fs.readFile(
 
     for (const line of lines) {
       lineNumber++;
-
-      if (!line.trim()) {
-        continue;
-      }
-
-      const words = line.trim().split(" ");
-      let colNumber = 1;
-
-      for (const word of words) {
-        if (!englishWords.includes(word)) {
-          console.log(
-            `Line ${lineNumber}, Col ${colNumber}: "${word}" appears to be a typo`,
-          );
-        }
-        colNumber += word.length + 1;
-      }
+      checkLine(line, lineNumber);
     }
   },
 );
+
+function checkLine(line: string, lineNumber: number): void {
+  line = cleanLine(line);
+
+  if (!line) {
+    return;
+  }
+
+  const words = line.split(/\s+/);
+
+  for (const word of words) {
+    if (checkWord(word)) {
+      console.error(`Line ${lineNumber}: "${word}" appears to be a typo`);
+    }
+  }
+}
+
+function cleanLine(line: string): string {
+  return line
+    .replaceAll("-", " ")
+    .replaceAll("—", " ")
+    .replaceAll("–", " ")
+    .replaceAll("'s", "")
+    .replaceAll("'s", "")
+    .replaceAll("/", " ")
+    .replaceAll("@", " ")
+    .replaceAll(/[^a-zA-Z' ]/g, "")
+    .trim();
+}
+
+function checkWord(word: string): boolean {
+  word = cleanWord(word);
+
+  return (
+    word !== "" &&
+    isNaN(Number(word.toLowerCase())) &&
+    !englishWords.includes(word.toLowerCase())
+  );
+}
+
+function cleanWord(word: string): string {
+  return word.replaceAll(/^['']+|['']+$/g, "").trim();
+}
